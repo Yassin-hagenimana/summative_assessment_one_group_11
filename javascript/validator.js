1bd400eb19220709d0e49366e1bc2f6a4e360cb4
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-06-19 10:25:46 +0200

    fix: correct date and phone regex

--- javascript/validator.js
+++ javascript/validator.js
@@ -1,10 +1,10 @@
 function eventFormValidationSchema(name, email, phoneNumber, eventDate, numberOfTickets) {
-    console.log("Validating form data...");
+    
     // Regular expressions for validation
     const nameRegex = /^[a-zA-Z ]+$/; // Allows letters and spaces only
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Validates email format
-    const phoneRegex = /^\d{12}$/; 
-    const dateRegex = /^(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])\/(19|20)\d{2}$/; // Validatedate in the format MM/DD/YYYY
+    const phoneRegex = /^\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/;
+    const dateRegex =  /^(?:(?:19|20)\d{2})-(?:(?:(?:0[13578]|1[02])-(?:0[1-9]|[12][0-9]|3[01]))|(?:(?:0[469]|11)-(?:0[1-9]|[12][0-9]|30))|(?:02-(?:0[1-9]|1[0-9]|2[0-8]))|(?:02-29))$/;    // Validate date in the format MM/DD/YYYY
     const ticketsRegex = /^[1-9]|10$/; // Validates number between 1 and 10
 
     // Object to store errors
